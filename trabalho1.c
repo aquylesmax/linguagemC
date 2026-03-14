03ad7af194847ee3797ace78dd013c1f498cf498
aquylesmax <aquyles2016@gmail.com>
2026-03-14 17:38:52 -0300

    novo codigo adicionado

---
+++ trabalho1.c
@@ -0,0 +1,12 @@
+#include<math.h>
+#include<stdio.h>
+
+int main() {
+    int numero, resultado;
+
+    printf("digite um numero: ");
+    scanf("%i", &numero);
+
+    resultado = numero * 3;
+    printf("%i", resultado);
+}
