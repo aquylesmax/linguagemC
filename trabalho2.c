066b8fae2cd641924d00b2b711d1ff9005de3151
aquylesmax <aquyles2016@gmail.com>
2026-03-15 12:35:19 -0300

    ajuste no arquivo tasks e lauch.json

---
+++ trabalho2.c
@@ -0,0 +1,13 @@
+#include<math.h>
+#include<stdio.h>
+
+int main() {
+
+    int numero, resultado;
+    
+    printf("digite um numero: ");
+    scanf("%i", &numero);
+
+    resultado=numero * numero;
+    printf("%i\n", resultado);
+}
